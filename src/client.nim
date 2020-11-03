abc5354462a5cdba451ac1c50956de7a5147398a
Sanaz <staheri14@ku.edu.tr>
2020-11-03 10:36:01 -0800

    adds the internal code of client

---
+++ src/client.nim
@@ -0,0 +1,13 @@
+##  client component of the chat application
+import os, system, threadpool
+
+if paramCount()==0:
+  quit ("Please specify the server address, e.g. ./client localhost")
+
+let serverAddr=paramStr(1)
+echo "connecting to ", serverAddr
+
+while true:
+  let message = spawn stdin.readLine() 
+  echo "Sending Message \"", ^ message, "\""
+echo "hello"
