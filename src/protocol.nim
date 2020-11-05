2ea66396a2c362577a7d75ec7eb2361626e7dd69
Sanaz <staheri14@ku.edu.tr>
2020-11-05 09:09:49 -0800

    adds unit tests for the parseMessage proc

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,7 +1,31 @@
+import json
 type
-  Message* = object 
-    username* : string
-    messsage* : string
+ Message* = object
+  username*: string
+  message*: string
 
 proc parseMessage(data: string): Message =
-  discard
+ let dataJson = parseJson(data) #returns a JsonNode
+ result.username = dataJson["username"].getStr()
+ result.message = dataJson["message"].getStr()
+
+
+
+when isMainModule:
+  block:
+    let data = """{ "username": "Dominik", "message":"What did you do for the weekend?" } """
+    let msg = parseMessage(data)
+    assert msg.message == "What did you do for the weekend?"
+    doAssert msg.username == "Dominik"
+  block:
+    let data = "invalid json"
+    try:
+      let msg = parseMessage(data)
+      doAssert false
+    except JsonParsingError:
+      doAssert true
+    except:
+      doAssert false
+    
+    
+
