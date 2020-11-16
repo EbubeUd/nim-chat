26d89572c3bc0bc9973c092a7e5d5f0f03dfd7d6
Sanaz <staheri14@ku.edu.tr>
2020-11-16 11:34:31 -0800

    adds the loop proc for receiving the client connections

--- src/server.nim
+++ src/server.nim
@@ -14,3 +14,21 @@ type
 
 proc newServer(): Server =
   Server(socket: newAsyncSocket(), clients: @[])
+
+proc loop(server: Server, port= 7687) {.async.} =
+  server.socket.bindAddr(Port(7687))
+  server.socket.listen()
+  # there should be a while true in here, removed out of curiousity 
+  let (netAddr, clientSocket) = await server.socket.acceptAddr()
+  echo "Accepted Connection from: ", netAddr
+  let client = Client(
+    socket: clientSocket,
+    netAddr: netAddr,
+    id: server.clients.len,
+    connected: true
+  )
+  server.clients.add(client)
+
+var server = newServer()
+waitFor server.loop()
+
