7f9cdd22fd1085369f571b3e73add31dee419bd4
Sanaz <staheri14@ku.edu.tr>
2020-11-17 11:23:19 -0800

    adds processMessages proc

--- src/server.nim
+++ src/server.nim
@@ -15,19 +15,34 @@ type
 proc newServer(): Server =
   Server(socket: newAsyncSocket(), clients: @[])
 
+
+proc processMessages(server: Server, client: Client) {.async.} = 
+  while true:
+    let line = await client.socket.recvLine()
+    if line.len == 0:
+      client.socket.close()
+      client.connected = false
+      echo "Connection closed!"
+      return
+    echo (client, " sent: ", line)
+    
+    
 proc loop(server: Server, port= 7687) {.async.} =
   server.socket.bindAddr(Port(7687))
   server.socket.listen()
   # there should be a while true in here, removed out of curiousity 
-  let (netAddr, clientSocket) = await server.socket.acceptAddr()
-  echo "Accepted Connection from: ", netAddr
-  let client = Client(
-    socket: clientSocket,
-    netAddr: netAddr,
-    id: server.clients.len,
-    connected: true
-  )
-  server.clients.add(client)
+  while true:
+    let (netAddr, clientSocket) = await server.socket.acceptAddr()
+    echo "Accepted Connection from: ", netAddr
+    let client = Client(
+      socket: clientSocket,
+      netAddr: netAddr,
+      id: server.clients.len,
+      connected: true
+    )
+    server.clients.add(client)
+    echo server.clients.len
+    asyncCheck processMessages(server, client)
 
 var server = newServer()
 waitFor server.loop()
