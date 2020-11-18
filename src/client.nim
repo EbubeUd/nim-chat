ccebc204d29daa6e8d6d00976adb974598469276
Sanaz <staheri14@ku.edu.tr>
2020-11-18 08:32:11 -0800

    adds network connection

--- src/client.nim
+++ src/client.nim
@@ -1,13 +1,25 @@
 ##  client component of the chat application
-import os, system, threadpool, asyncdispatch, asyncnet
+import os, system, threadpool, asyncdispatch, asyncnet, protocol
+
+proc connect(socket: AsyncSocket, serverAddr: string ) = 
+  echo("connecting to ", serverAddr)
+  await socket.connect(serverAddr, 7687.Port)
+  echo("Connected!")
+
+  while true:
+    let line = socket.recvLine()
+    let parsed = parseMessage(line)
+    echo(parsed.username, " said ", parsed.message)
 
 if paramCount()==0:
   quit ("Please specify the server address, e.g. ./client localhost")
 
 let serverAddr=paramStr(1)
-echo "connecting to ", serverAddr
+let socket = newAsyncSocket()
+
 
 while true:
   let message = spawn stdin.readLine() 
   echo "Sending Message \"", ^ message, "\""
 echo "hello"
+
