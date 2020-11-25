c9ea4163ebd60fff07fa12170af6edd09cf77bd3
Sanaz <staheri14@ku.edu.tr>
2020-11-25 09:15:00 -0800

    adds comments and fxes a connection bug

--- src/client.nim
+++ src/client.nim
@@ -1,5 +1,5 @@
 ##  client component of the chat application
-import os, system,  asyncdispatch, asyncnet, protocol
+import os, system,  threadpool, asyncdispatch, asyncnet, protocol
 
 proc connect(socket: AsyncSocket, serverAddr: string ) {.async.} = 
   echo("connecting to ", serverAddr)
@@ -12,16 +12,24 @@ proc connect(socket: AsyncSocket, serverAddr: string ) {.async.} =
     echo(parsed.username, " said ", parsed.message)
 
 
-if paramCount()==0:
-  quit ("Please specify the server address, e.g. ./client localhost")
+if paramCount()!=2:
+  quit ("Please specify the server address, e.g. ./client localhost followed by your chat name")
 
-let serverAddr=paramStr(1) # read the server address from command line input
+let serverAddr = paramStr(1) # read the server address from command line input
+let name = paramStr(2)#  get the user name for the chat application
+
 var socket = newAsyncSocket() # create a socket to connect to the server
-
 asyncCheck connect(socket, serverAddr) # connect to the server and start listening
 
-let message = spawn await stdi.readLine() # use spawn to create a new thread
+
+
+var messageFlowVar = spawn stdin.readLine() # use spawn to create a new thread, message is of type Future holding the string returned by readLine
 while true:
-  echo ("sending \"", ^message , "\"") # message is of Future[T] type and can be accessed by ^ operator
+  if messageFlowVar.isReady():
+    let message = CreateMessage(name, ^messageFlowVar)
+    # echo ("sending \"", message , "\"") # message is of Future[T] type and can be accessed by ^ operator
+    asyncCheck socket.send(message)
+    messageFlowVar = spawn stdin.readLine()
+  asyncdispatch.poll()
 
 
